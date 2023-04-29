171287833522e77ee1f4fbb9383d857cfb870149
UnsignedArduino <UnsignedArduino@outlook.com>
2023-04-28 22:48:02 -0400

    Serve moves

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 import aiohttp
 import arrow
+import chess.pgn
+from io import StringIO
 
 app = FastAPI()
 
 JSON_URL = "https://tcec-chess.com/live.json"
+PGN_URL = "https://tcec-chess.com/live.pgn"
 TIMEZONE = "America/New_York"
 
 
@@ -13,9 +17,9 @@ TIMEZONE = "America/New_York"
 async def route_metadata():
     async with aiohttp.ClientSession() as session:
         async with session.get(JSON_URL) as response:
-            live = await response.json()
+            metadata = await response.json()
 
-    headers = live["Headers"]
+    headers = metadata["Headers"]
 
     start_time = arrow.get(headers["GameStartTime"].replace(" UTC", "Z")).to(
         TIMEZONE)
@@ -47,6 +51,19 @@ async def route_metadata():
     }
 
     return wanted
+
+
+@app.get("/moves", response_class=PlainTextResponse)
+async def route_moves():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(PGN_URL) as response:
+            pgn = await response.text()
+
+    game = chess.pgn.read_game(StringIO(pgn))
+    exporter = chess.pgn.StringExporter(columns=None, headers=False,
+                                        comments=False, variations=False)
+
+    return game.accept(exporter)
 
 
 if __name__ == "__main__":
