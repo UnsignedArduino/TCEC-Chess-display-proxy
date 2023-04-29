43b318bfda3915fdf6e5c7bf16be6a5a9c541455
UnsignedArduino <UnsignedArduino@outlook.com>
2023-04-29 19:02:30 -0400

    Bro is using selenium to scrape the web page damnnnn

--- src/main.py
+++ src/main.py
@@ -5,9 +5,15 @@ import aiohttp
 import arrow
 import chess.pgn
 from io import StringIO
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 app = FastAPI()
+service = ChromeService(executable_path=ChromeDriverManager().install())
 
+SCRAPER_URL = "https://tcec-chess.com/"
 JSON_URL = "https://tcec-chess.com/live.json"
 PGN_URL = "https://tcec-chess.com/live.pgn"
 TIMEZONE = "America/New_York"
@@ -15,6 +21,10 @@ TIMEZONE = "America/New_York"
 
 @app.get("/metadata")
 async def route_metadata():
+    driver = webdriver.Chrome(service=service)
+    driver.implicitly_wait(10)
+    driver.get(SCRAPER_URL)
+
     async with aiohttp.ClientSession() as session:
         async with session.get(JSON_URL) as response:
             metadata = await response.json()
@@ -26,18 +36,32 @@ async def route_metadata():
     time_control = int(headers["TimeControl"].split("+")[0])
     time_bonus = int(headers["TimeControl"].split("+")[1])
 
+    get_ele = lambda id: driver.find_element(by=By.ID, value=id)
+
     wanted = {
         "event": {
             "name": headers["Event"],
             "round": headers["Round"]
         },
         "white": {
-            "name": headers["White"],
-            "elo": headers["WhiteElo"]
+            "name": headers["White"].split(" ")[0],
+            "version": headers["White"].split(" ")[1],
+            "elo": headers["WhiteElo"],
+            "eval": float(get_ele("eval0").text),
+            "depth": get_ele("depth0").text,
+            "speed": get_ele("speed0").text,
+            "nodes": get_ele("node0").text,
+            "remaining": get_ele("remain0").text
         },
         "black": {
-            "name": headers["Black"],
-            "elo": headers["BlackElo"]
+            "name": headers["Black"].split(" ")[0],
+            "version": headers["Black"].split(" ")[1],
+            "elo": headers["BlackElo"],
+            "eval": float(get_ele("eval1").text),
+            "depth": get_ele("depth1").text,
+            "speed": get_ele("speed1").text,
+            "nodes": get_ele("node1").text,
+            "remaining": get_ele("remain1").text
         },
         "game": {
             "start_absolute": start_time.format("HH:mm:ss MM/DD/YYYY"),
@@ -47,8 +71,10 @@ async def route_metadata():
             "time_control": "{0} + {1}".format(
                 arrow.get(time_control).format("H:mm:ss"),
                 arrow.get(time_bonus).format("m:ss"))
-        }
+        },
     }
+
+    driver.quit()
 
     return wanted
 
